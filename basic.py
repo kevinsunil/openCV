c08dc061ea37bd63189e6208e25d2713d96b57fc
kevinsunil <kevinsunil.2001@gmail.com>
2022-04-08 23:51:40 +0530

    .

--- basic.py
+++ basic.py
@@ -11,4 +11,19 @@ cv.imshow('Grayscaled image', gray)
 blur = cv.GaussianBlur(img,(3,3),cv.BORDER_DEFAULT)
 cv.imshow('BLUR', blur)
 
+#Edge  Cascade
+canny = cv.Canny(blur,125,175)
+cv.imshow('Canny Edges', canny)
+
+#Dilating the image
+dilated = cv.dilate(canny, (3,3) , iterations = 1)
+cv.imshow('Dilated', dilated)
+
+#Eroding
+eroded = cv.erode(dilated,(3,3), iterations=1)
+cv.imshow('Eroded', eroded)
+
+#Resize
+resized =cv.resize(img, (500,500), interpolation = cv.INTER_CUBIC)
+cv.imshow('Resized', resize)
 cv.waitKey(0)
